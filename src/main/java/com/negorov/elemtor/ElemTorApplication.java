741df6ba149718fa74b3ae2cd7357ba2729b4c09
kolyanovna <egorkamilev@gmail.com>
2017-07-22 18:12:00 +0300

    first step

---
+++ src/main/java/com/negorov/elemtor/ElemTorApplication.java
@@ -0,0 +1,12 @@
+package com.negorov.elemtor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElemTorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElemTorApplication.class, args);
+	}
+}
